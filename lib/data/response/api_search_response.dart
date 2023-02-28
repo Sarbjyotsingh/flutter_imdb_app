c54d897a0392815b5d740803ace584f7b279d44b
Sarbjyot Singh Chahal <chahalsarbjyotsingh@gmail.com>
2023-02-28 18:23:45 +0100

    Converting api response to APIResponseModel

--- lib/data/response/api_search_response.dart
+++ lib/data/response/api_search_response.dart
@@ -1,16 +1,18 @@
 import 'package:flutter_imdb_app/data/network/network_api_service.dart';
+import 'package:flutter_imdb_app/models/api_repsonse_model/api_response_model.dart';
 
 class MovieSearchAPI {
   final String _apiEndPoint = 'https://www.omdbapi.com';
   final String _apiKey = '489182a9';
-  Future getMovieSearchResponse(String searchText) async {
-    Map<String, dynamic> apiResponse;
+
+  Future<APIResponseModel> getMovieSearchResponse(String searchText) async {
+    APIResponseModel apiResponse;
     if (searchText.isNotEmpty) {
       String fetchUrl = '$_apiEndPoint/?s={$searchText}&apikey=$_apiKey';
 
       apiResponse = await NetworkApiService().getGetApiResponse(fetchUrl);
     } else {
-      apiResponse = {};
+      apiResponse = APIResponseModel();
     }
     return apiResponse;
   }
