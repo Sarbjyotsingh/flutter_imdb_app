fafda996b53d8ef73a58da28cac516b65c120eb9
Sarbjyot Singh Chahal <chahalsarbjyotsingh@gmail.com>
2023-02-28 17:18:37 +0100

    Adding Colors, Constants and Style in Utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// App Bar Elevation
+const double kAppBarElevation = 0;
+
+// Paddings
+const EdgeInsets kScreenPadding = EdgeInsets.all(10);
---
+++ lib/utils/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_imdb_app/utils/colors.dart';
+
+const TextStyle kTextStyle = TextStyle(
+  color: kWhiteColor,
+);
