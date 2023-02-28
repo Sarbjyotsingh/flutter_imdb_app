1b4cf2482ef205237446c662c3cb53e1080f7714
Sarbjyot Singh Chahal <chahalsarbjyotsingh@gmail.com>
2023-02-28 18:14:02 +0100

    Fix flutter format

--- lib/models/api_repsonse_model/api_response_model.dart
+++ lib/models/api_repsonse_model/api_response_model.dart
@@ -7,10 +7,10 @@ part 'api_response_model.g.dart';
 class APIResponseModel {
   int? totalResults;
 
-  @JsonKey(name:'Search')
+  @JsonKey(name: 'Search')
   List<MovieModel> search;
 
-  @JsonKey(name:'Response')
+  @JsonKey(name: 'Response')
   String? response;
 
   APIResponseModel({
